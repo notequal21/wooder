58d8d4f05f8c15fcea91fe0625acbc23eb684e85
notequal21 <74978665+notequal21@users.noreply.github.com>
2021-03-09 20:32:29 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -70,6 +70,20 @@ if (animItems.length > 0) {
         animOnScroll();
     }, 300);
 }
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
+}
 
 // ---- JQuery
 
